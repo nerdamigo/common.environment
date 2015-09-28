2ae880e9ff2f9aa247e268857c183c9ad6ad6b5e
matt <matt@nerdamigo.com>
2015-09-28 12:52:10 -0700

    initial

---
+++ HostNameResolver.cs
@@ -0,0 +1,26 @@
+﻿using NerdAmigo.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdAmigo.Common.Environment
+{
+    public class HostNameResolver : IHostNameResolver
+    {
+		public string GetHostName()
+		{
+			string dnsHostName = System.Net.Dns.GetHostName();
+			string machineName = System.Environment.MachineName;
+
+			string hostName = dnsHostName;
+			if (String.IsNullOrEmpty(hostName))
+			{
+				hostName = machineName;
+			}
+
+			return hostName.ToLower();
+		}
+	}
+}
